ce7d9cd5186ab563a684274c100b005d534f89ea
Mike Hodnick <mike@kindohm.com>
2022-04-19 20:48:25 -0500

    wip

---
+++ experiments/node/bootTidal.hs
@@ -0,0 +1,58 @@
+:set -XOverloadedStrings
+:set prompt ""
+
+import Sound.Tidal.Context
+
+import System.IO (hSetEncoding, stdout, utf8)
+hSetEncoding stdout utf8
+
+-- total latency = oLatency + cFrameTimespan
+tidal <- startTidal (superdirtTarget {oLatency = 0.1, oAddress = "127.0.0.1", oPort = 57120}) (defaultConfig {cVerbose = True, cFrameTimespan = 1/20})
+
+:{
+let only = (hush >>)
+    p = streamReplace tidal
+    hush = streamHush tidal
+    list = streamList tidal
+    mute = streamMute tidal
+    unmute = streamUnmute tidal
+    unmuteAll = streamUnmuteAll tidal
+    unsoloAll = streamUnsoloAll tidal
+    solo = streamSolo tidal
+    unsolo = streamUnsolo tidal
+    once = streamOnce tidal
+    first = streamFirst tidal
+    asap = once
+    nudgeAll = streamNudgeAll tidal
+    all = streamAll tidal
+    resetCycles = streamResetCycles tidal
+    setcps = asap . cps
+    getcps = streamGetcps tidal
+    getnow = streamGetnow tidal
+    d1 = p 1 . (|< orbit 0)
+    d2 = p 2 . (|< orbit 1)
+    d3 = p 3 . (|< orbit 2)
+    d4 = p 4 . (|< orbit 3)
+    d5 = p 5 . (|< orbit 4)
+    d6 = p 6 . (|< orbit 5)
+    d7 = p 7 . (|< orbit 6)
+    d8 = p 8 . (|< orbit 7)
+    d9 = p 9 . (|< orbit 8)
+    d10 = p 10 . (|< orbit 9)
+    d11 = p 11 . (|< orbit 10)
+    d12 = p 12 . (|< orbit 11)
+:}
+
+:{
+let getState = streamGet tidal
+    setI = streamSetI tidal
+    setF = streamSetF tidal
+    setS = streamSetS tidal
+    setR = streamSetR tidal
+    setB = streamSetB tidal
+:}
+
+:set prompt "tidal> "
+:set prompt-cont ""
+
+default (Pattern String, Integer, Double)
