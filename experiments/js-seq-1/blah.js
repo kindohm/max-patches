bdcb32a51e5771cfdfd4355e8fb12cef1245d216
Mike Hodnick <mike@kindohm.com>
2022-05-12 20:44:50 -0500

    add part 1 fills

--- experiments/js-seq-1/blah.js
+++ experiments/js-seq-1/blah.js
@@ -9,6 +9,8 @@ var eventTypes = [1,2];
 var restType = 0;
 var event2Probability = 25;
 var weightedEventTypes = [1,2];
+var fill1aOn = false;
+var fill1bOn = false;
 
 var weights = {
 	'4n': 1,
@@ -20,7 +22,7 @@ var weights = {
 function randInt(min, maxx) {
   min = Math.ceil(min);
   maxx = Math.floor(maxx);
-  return Math.floor(Math.random() * (maxx - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (maxx - min + 1) + min);
 }
 
 function randLength(lengths) {
@@ -49,27 +51,134 @@ function bang(){
 }
 
 function generate(){
+	var events = [];
 	var prev = [0,0];
-	
 	var eventLengthsWithWeights = getCurrentEventWeightsWithLengths();
+	
 		
 	for (var i = 0; i < numEvents; i++){
 		var len, chan;
 		var prevLen = prev[0];
-		var prevChan = prev[1];
-		var repeat = i > 0 && prevChan !== 0 && Math.random() < percentRepeat / 100;
+		var prevType = prev[1];
+		var repeat = i > 0 && prevType !== 0 && Math.random() < percentRepeat / 100;
 		
 		if (repeat){
 			len = prevLen
-			chan = prevChan;
+			type = prevType;
 		} else {
 			var rest = Math.random() < percentRest/100;
 			len = randLength(eventLengthsWithWeights);
-			chan = rest ? restType : randType();
+			type = rest ? restType : randType();
 		}
 		prev = [len, chan];
-		outlet(0, i, len, chan);
+		events.push({index: i, len: len, type: type});
 	}
+	
+	post('fill1aOn', fill1aOn, '\n');
+	post('fill1bOn', fill1bOn, '\n');
+	
+	post('original', '\n');
+	logColl(events);
+	
+	events = addFills(events);
+	
+	post('new', '\n');
+	logColl(events);
+	
+	
+	for (var i = 0; i < events.length; i++){
+		outlet(0, events[i].index, events[i].len, events[i].type);
+	}	
+}
+
+function logColl(events){
+	for (var i = 0; i < events.length; i++){
+		post(events[i].index, events[i].len, events[i].type);
+		post(', ');
+	}
+	post('\n');
+}
+
+function addFills(events){
+	var newEvents = [];
+	var newIndex = 0;
+	
+	
+	for (var i = 0; i < events.length; i++){
+		var len = events[i].len;
+		var type = events[i].type;
+		
+		if (type === 2 || len === '16n' || (!fill1aOn && !fill1bOn)) {
+			var modifiedEvent = { index: newIndex, len: len, type: type };
+			newIndex++;
+			newEvents.push(modifiedEvent);
+		} else if (len === '4n' && type === 1 && fill1aOn && fill1bOn) {
+			var modifiedEvent = { index: newIndex, len: '16n', type: type };
+			newIndex++;
+			var fillAEvent = { index: newIndex, len: '16n', type: 3 };
+			newIndex++;
+			var fillBEvent = { index: newIndex, len: '8n', type: 4 };
+			newIndex++;
+			newEvents.push(modifiedEvent);
+			newEvents.push(fillAEvent);
+			newEvents.push(fillBEvent);
+		} else if (len === '8dn' && type === 1 && fill1aOn && fill1bOn) {
+			var modifiedEvent = { index: newIndex, len: '16n', type: type };
+			newIndex++;
+			var fillAEvent = { index: newIndex, len: '16n', type: 3 };
+			newIndex++;
+			var fillBEvent = { index: newIndex, len: '16n', type: 4 };
+			newIndex++;
+			newEvents.push(modifiedEvent);
+			newEvents.push(fillAEvent);
+			newEvents.push(fillBEvent);
+		} else if (len === '8n' && type === 1 && fill1aOn) {
+			var modifiedEvent = { index: newIndex, len: '16n', type: type };
+			newIndex++;
+			var fillAEvent = { index: newIndex, len: '16n', type: 3 };
+			newIndex++;
+			newEvents.push(modifiedEvent);
+			newEvents.push(fillAEvent);
+		} else if (len === '4n' && type === 1 && fill1aOn && !fill1bOn) {
+			var modifiedEvent = { index: newIndex, len: '16n', type: type };
+			newIndex++;
+			var fillAEvent = { index: newIndex, len: '8dn', type: 3 };
+			newIndex++;
+			newEvents.push(modifiedEvent);
+			newEvents.push(fillAEvent);
+		} else if (len === '8dn' && type === 1 && fill1aOn && !fill1bOn) {
+			var modifiedEvent = { index: newIndex, len: '16n', type: type };
+			newIndex++;
+			var fillAEvent = { index: newIndex, len: '8n', type: 3 };
+			newIndex++;
+			newEvents.push(modifiedEvent);
+			newEvents.push(fillAEvent);
+		} else if (len === '8n' && type === 1 && fill1aOn && !fill1bOn) {
+			var modifiedEvent = { index: newIndex, len: '16n', type: type };
+			newIndex++;
+			var fillAEvent = { index: newIndex, len: '16n', type: 3 };
+			newIndex++;
+			newEvents.push(modifiedEvent);
+			newEvents.push(fillAEvent);
+		} else if (len === '4n' && type === 1 && !fill1aOn && fill1bOn) {
+			var modifiedEvent = { index: newIndex, len: '8n', type: type };
+			newIndex++;
+			var fillBEvent = { index: newIndex, len: '8n', type: 4 };
+			newIndex++;
+			newEvents.push(modifiedEvent);
+			newEvents.push(fillBEvent);
+		} else if (len === '8dn' && type === 1 && !fill1aOn && fill1bOn) {
+			var modifiedEvent = { index: newIndex, len: '8n', type: type };
+			newIndex++;
+			var fillBEvent = { index: newIndex, len: '16n', type: 4 };
+			newIndex++;
+			newEvents.push(modifiedEvent);
+			newEvents.push(fillBEvent);
+		}		
+
+	}
+	
+	return newEvents;
 }
 
 
@@ -129,3 +238,12 @@ function updateWeightedEventTypes(){
 	weightedEventTypes = newValues;
 }
 
+function fill1a(val){
+	fill1aOn = val === 1;
+	generate();
+}
+
+function fill1b(val){
+	fill1bOn = val === 1;
+	generate();
+}
